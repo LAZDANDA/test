835a32a0e4931297105107f5c69a9a0f746eb12a
LAZDANDA <130457228+LAZDANDA@users.noreply.github.com>
2023-04-25 12:42:28 +0500

    first commint

--- 1 lesson/main.js
+++ 1 lesson/main.js
@@ -1,5 +1,25 @@
-console.log("Hello World");
-console.log(123);
-console.log("Hello"+ "World");
+// console.log("Hello World");
+// console.log(123);
+// console.log("Hello"+ "World");
 // alert("Tne Wardo!!!");
-document.getElementById("first").innerHTML="Привет Анна!"
+// document.getElementById("first").innerHTML= "<h1>Доброе утро!</h1>"
+// let jojo ="jojo"
+// jojo="DOOR"
+// console.log (jojo)
+// document. querySelector ("#one").innerHTML= 666;
+// document.querySelector("p").innerHTML="ВЕСНА СТИХ!"
+let inputIN=document.querySelector("text")
+let btn=document.querySelector("chek-in")
+    btn.onclick= function () {
+        console.log("DA")
+        console.log (inputIN.value)
+    }
+
+let div= document.querySelector("out")
+
+     btn.onclick= function () {
+        console.log("DA")
+        console.log (inputIN.value)
+        let e = +inputIN.value
+        console.log(e)
+    }
