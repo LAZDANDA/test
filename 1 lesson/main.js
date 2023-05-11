2f7092ce6154fee708e53d038d1395a0bede401e
LAZDANDA <130457228+LAZDANDA@users.noreply.github.com>
2023-05-11 15:22:04 +0500

    first commint

--- 1 lesson/main.js
+++ 1 lesson/main.js
@@ -9,18 +9,26 @@
 // document. querySelector ("#one").innerHTML= 666;
 // document.querySelector("p").innerHTML="ВЕСНА СТИХ!"
 
-let inputIN = document.querySelector("text")
-let btn = document.querySelector("chek-in")
-    btn .onclick = function () {
-        console.log("DA")   
-    }
+// const inputIN = document.querySelector(".text");
+// const btn = document.querySelector(".chek-in");
+    // btn .onclick = function () {
+        // console.log("DA")   
+    // }
 
- let div= document.querySelector("out")
+// const div = document.querySelector(".out");
 
-      btn.onclick= function () {
-         console.log("DA")
-         console.log (inputIN.value)
-         let e = +inputIN.value
-         console.log(e)
-         div.innerHTML = e
-     }
+    //   btn.onclick= function () {
+        //  console.log("DA")
+        //  console.log (inputIN.value)
+        //  let e = +inputIN.value
+        //  console.log(e)
+        //  div.innerHTML = e
+    //  }
+const one = document.querySelector(".one")
+      one.style.color = "red"
+      one.style.paddingLeft = "150px"
+      one.classList.add("black")
+
+
+     
+
